5496e6b44b94ca961aa6fcd2427ba3c40b701914
sharkboy0555 <zchambers@zimsbagging.com>
2025-07-17 09:27:03 -0400

    Update machine.php

--- machine.php
+++ machine.php
@@ -1,6 +1,4 @@
 <?php
-require_once dirname(__FILE__) . '/fpp_plugin.php';
-
 class MachinePlugin extends FPPPlugin {
     public function __construct() {
         parent::__construct();
@@ -19,7 +17,7 @@ class MachinePlugin extends FPPPlugin {
             $text  = $settings[$field] ?? '';
             $color = $settings['color'] ?? '#FFFFFF';
             $this->drawText($text, 'fixed', 0, $y, $color);
-            $y += 12;
+            $y += 12;  // line height
         }
     }
 }
